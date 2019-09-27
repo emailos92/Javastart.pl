9be02d0ad2d8e9a6108edd4235f0b2709835698f
Emil <emailo1992@gmail.com>
2019-09-27 18:58:52 +0200

    Add biblioteka project, checkpoint 1 - zmienne

---
+++ biblioteka/src/Library.java
@@ -0,0 +1,21 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka V0.1";
+
+        String title = "James Bond - agent 007";
+        String author = "Cornel Egmont";
+        int releaseDate = 2019;
+        int pages = 412;
+        String publisher = "Dobra Książka";
+        String isbn = "838231241";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
